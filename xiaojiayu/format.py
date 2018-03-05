b45cfc93a6f1d0bf9df9979ca1246a13842e0ad0
qsort987 <prj_share@sina.com>
2018-03-05 19:27:18 +0800

    script: print format and list
    add format.py
    add list.py
    
    Signed-off-by: qsort987 <prj_share@sina.com>

---
+++ xiaojiayu/format.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+print "%c" % (97)
+print "%c" % 98
+print "%-10d" % 68
+print "%10d"  % 69
+
+print "%010d" % 67
+print "%2f" % 78.96
+
+# location par
+print "{0} love {1} {2}".format("I", "ubuntu", "too")
+
+print "{a}, love {b} {c}".format(a="I", b="ubuntu", c="also")
+
+print "Total: {0:.1f}{1}".format(27.66, "GB")
