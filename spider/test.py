37067e032b1a200d222bd418182b14d63be9aba9
qsort987 <prj_share@sina.com>
2018-03-16 08:00:43 +0800

    script: get the donwlonad link
    improve the test.py to get the Image link
    
    Signed-off-by: qsort987 <prj_share@sina.com>

--- spider/test.py
+++ spider/test.py
@@ -19,14 +19,18 @@ for img in soup.find_all(src="*"):
 
 def getPicture(url):
     resp = urllib.request.urlopen(url)
-    soup = BeautifulSoup(resp, 'lxml')
+    #soup = BeautifulSoup(resp, 'lxml')
+    soup = BeautifulSoup(resp)
     #print (soup)
-    pattern = re.compile(".gif$")
+    pattern = re.compile("data-original")
     for tag in soup.find_all(re.compile("^img")):
-        print (tag.attrs)
+        #print (tag.attrs)
         #print (tag.attrs['src'])
         #print (tag.attrs['data-original'])
-        #print (re.search('.gif$', tag.attrs))
+        #print (re.search(pattern, str(tag.attrs)))
+
+        if re.search(pattern, str(tag.attrs)):
+            print (tag.attrs['data-original'])
 """
         if tag.attrs['src'] == re.compile("$.gif"):
             print (tag.attrs['data-original'])
